cfa07a21d0cb1b33740a296e00bbaa20b467c0a2
WebSchematix <webschematix@gmail.com>
2022-01-26 00:54:02 -0500

    Add files via upload

---
+++ wp-blanktheme/functions.php
@@ -0,0 +1,4 @@
+<?php
+ // nothing here
+ // add functions with a plugin like Code Snippets (https://wordpress.org/plugins/code-snippets/), Advanced Scripts (https://www.cleanplugins.com/products/advanced-scripts/), or Scripts Organizer (https://docs.dplugins.com/scripts-organizer/scripts-organizer/)
+?>
