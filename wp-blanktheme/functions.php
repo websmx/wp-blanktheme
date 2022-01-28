95c91052aea79308fdb700281a0c479432e908a9
WebSchematix <webschematix@gmail.com>
2022-01-28 11:31:14 -0500

    removed ending ?>

--- wp-blanktheme/functions.php
+++ wp-blanktheme/functions.php
@@ -1,4 +1,3 @@
 <?php
  // nothing here
  // add functions with a plugin like Code Snippets (https://wordpress.org/plugins/code-snippets/), Advanced Scripts (https://www.cleanplugins.com/products/advanced-scripts/), or Scripts Organizer (https://docs.dplugins.com/scripts-organizer/scripts-organizer/)
-?>
